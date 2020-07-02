b203da9d84d2be01d3d97a3e91ead5ec6c9e0873
M. Lane Thompson <65419580+wtrboy@users.noreply.github.com>
2020-07-02 10:08:44 -0400

    Add files via upload

---
+++ Assignment1/assignment1.js
@@ -0,0 +1,13 @@
+var numbers = [10,2,3,4,5,6];
+
+// sum the numbers and save it in a var
+
+var total = numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5];
+
+// calculate the average
+
+var average = total/6;
+
+// display alert message with result
+
+alert("The average is "+average);
